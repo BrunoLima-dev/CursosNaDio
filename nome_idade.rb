a2bf78a8a08287a94b24aee8eb9632c19aeb76ca
BrunoLima-dev <brunoprograme04@gmail.com>
2023-12-09 18:34:12 -0300

    Remove unnecessary Ruby files

--- nome_idade.rb
+++
@@ -1,7 +0,0 @@
-puts 'Escreva seu primera nome:'
-first_name = gets.chomp
-puts 'Escreva seu segundo nome:'
-last_name = gets.chomp
-puts 'Escreva a sua idade:'
-idade = gets.chomp
-puts "Nome: #{first_name} #{last_name} idade: #{idade} anos"
